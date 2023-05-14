9a886395b145556872edaca66b130eec12315166
juan <juan@juan>
2023-05-14 20:01:49 -0300

    html skeleton finished

---
+++ src/index.js
@@ -0,0 +1,26 @@
+//import './styles.css'
+
+let lastProjectId = 1;
+
+/**
+ 
+ 
+
+let matchProj = (idToSearch) => {
+
+    for (let val in Projects){
+
+        if(idToSearch == Projects[val].projId){
+
+            console.log('match ' + idToSearch + ' ' + Projects[val].projId);
+
+            projToShow = Projects[val];
+
+            console.log(projToShow.tasks[val].taskInfo);
+
+            return Projects[val].projId;
+
+        }
+
+
+ */
